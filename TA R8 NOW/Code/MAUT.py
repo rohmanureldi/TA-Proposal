e3f6503240777b3ae421183a569c074f87408271
rohmanureldi <eldirohmanur@gmail.com>
2019-04-22 00:18:14 +0700

    initial

--- TA R8 NOW/Code/MAUT.py
+++ TA R8 NOW/Code/MAUT.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_excel (r'Lembar Survey.xlsx','Lembar Survey',index_col=None, na_values=['NA'], usecols = "G,H,I") 
+data = pd.read_excel (r'Lembar Survey.xlsx','Lembar Survey',index_col=None, na_values=['NA'], usecols = "G:Q") 
 data.columns
 df = pd.DataFrame(data)
 
@@ -9,26 +9,51 @@ pelayanan_index = df.iloc[30:50,0].reset_index(drop=True)
 pengelolaan_index = df.iloc[50:52,0].reset_index(drop=True)
 
 
-produk_value=df.iloc[3:30,1].reset_index(drop=True)
-pelayanan_value = df.iloc[30:50,1].reset_index(drop=True)
-pengelolaan_value = df.iloc[50:52,1].reset_index(drop=True)
+produk_value=pd.DataFrame()
+pelayanan_value=pd.DataFrame()
+pengelolaan_value=pd.DataFrame()
 
-produk_value2=df.iloc[3:30,2].reset_index(drop=True)
-pelayanan_value2=df.iloc[30:50,2].reset_index(drop=True)
-pengelolaan_value2=df.iloc[50:52,2].reset_index(drop=True)
 
-produk_value=pd.concat([produk_value,produk_value2],ignore_index=True,axis=1)
-pelayanan_value=pd.concat([produk_value,pelayanan_value2],ignore_index=True,axis=1)
-pengelolaan_value=pd.concat([produk_value,pengelolaan_value2],ignore_index=True,axis=1)
+for i in range(1,df.shape[1]):
+    produk_value.insert(i-1,i-1,(df.iloc[3:30,i].reset_index(drop=True)).values.astype(int))
+    pelayanan_value.insert(i-1,i-1,(df.iloc[30:50,i].reset_index(drop=True)).values.astype(int))
+    pengelolaan_value.insert(i-1,i-1,(df.iloc[50:52,i].reset_index(drop=True)).values.astype(int))
+#    produk_matrix.insert(i-1,i-1,produk_value[i])
 
-def hitung_bobot(index,value,j):
+#produk_value=df.iloc[3:30,1].reset_index(drop=True)
+#pelayanan_value = df.iloc[30:50,1].reset_index(drop=True)
+#pengelolaan_value = df.iloc[50:52,1].reset_index(drop=True)
+#
+#produk_value2=df.iloc[3:30,2].reset_index(drop=True)
+#pelayanan_value2=df.iloc[30:50,2].reset_index(drop=True)
+#pengelolaan_value2=df.iloc[50:52,2].reset_index(drop=True)
+#
+#produk_value=pd.concat([produk_value,produk_value2],ignore_index=True,axis=1)
+##pelayanan_value=pd.concat([produk_value,pelayanan_value2],ignore_index=True,axis=1)
+#pengelolaan_value=pd.concat([produk_value,pengelolaan_value2],ignore_index=True,axis=1)
+    
+print(produk_value[0][3])
+print(produk_index[0])
+
+def hitung_bobot(index,listValue,j):
     sum=0
     for i in range (0, len(index)):
-        if(index[i]=="M" and value[j][i]==1):
+        if(index[i]=="M" and listValue[j][i]==1):
             sum+=1
-        if(index[i]=="TM" and value[j][i]==1):
+        if(index[i]=="TM" and listValue[j][i]==1):
             sum+=0.5
     return sum
+
+
+print(hitung_bobot(produk_index,produk_value,0))
+
+produk_bobot=pd.DataFrame()
+pelayanan_bobot=pd.DataFrame()
+pengelolaan_bobot=pd.DataFrame()
+for k in range(0,produk_value.shape[1]):
+    produk_bobot.insert(k,('hotel '+str(k+1)),[(hitung_bobot(produk_index,produk_value,k))/20.5])
+    pelayanan_bobot.insert(k,('hotel '+str(k+1)),[(hitung_bobot(pelayanan_index,pelayanan_value,k))/14])
+    pengelolaan_bobot.insert(k,('hotel '+str(k+1)),[(hitung_bobot(pengelolaan_index,pengelolaan_value,k))/2])
 
 
 produk_bobot=((hitung_bobot(produk_index,produk_value,0))/20.5)
@@ -38,6 +63,9 @@ pengelolaan_bobot=((hitung_bobot(pengelolaan_index,pengelolaan_value,0))/2)
 produk_bobot2=(hitung_bobot(produk_index,produk_value,1)/20.5)
 pelayanan_bobot2=(hitung_bobot(pelayanan_index,pelayanan_value,1)/14)
 pengelolaan_bobot2=(hitung_bobot(pengelolaan_index,pengelolaan_value,1)/2)
+
+
+
 preferensi={
         'produk':0.561644,
         'pelayanan':0.383562,
@@ -53,7 +81,7 @@ pembobotan = pd.DataFrame({
 
 pembobotan=pembobotan.set_index('alternatif')
 
-print(pembobotan.index[0])
+print(len(pembobotan))
 
 def normalisasi():
     hasil_normalisasi_array={}
